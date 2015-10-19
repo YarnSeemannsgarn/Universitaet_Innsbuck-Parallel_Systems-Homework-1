deaeec2a7db8a2515deb988f476a39c132c6bc79
YarnSeemannsgarn <janschlenker1990@gmail.com>
2015-10-20 01:23:58 +0200

    Add gaussian elimination

--- programme/main.c
+++ programme/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "sequential_algorithms.h"
 
-int **call_matrix_multiplication(size_t N){
+int** call_matrix_multiplication(size_t N){
   int matrix1[N][N];
   int matrix2[N][N];
   int i, j;
@@ -19,6 +19,26 @@ int **call_matrix_multiplication(size_t N){
   return matrix_multiplication(&matrix1[0][0], &matrix2[0][0], N, N, N, N);  
 }
 
+double* call_gaussian_elimination(size_t N){
+  double a[N*N];
+  double b[N];
+  double* x = calloc(N, sizeof(double));
+  double random_double;
+  int i, j;
+
+  for(i=0; i<N; ++i){
+    random_double = ((double)rand()/(double)RAND_MAX);
+    b[i] = random_double;
+    for(j=0; j<N; ++j){
+      random_double = ((double)rand()/(double)RAND_MAX);
+      a[i*N+j] = random_double;
+    }
+  }
+
+  gaussian_elimination(a, b, x, N);
+  return x;
+}
+
 int main(int argc, char* argv[]){
   if(argc != 2){
     printf("Usage: %s <array-dimension>\n", argv[0]);
@@ -29,7 +49,10 @@ int main(int argc, char* argv[]){
   srand(time(NULL));
 
   int** result_matrix_multiplication = call_matrix_multiplication(N);
-  destroyMatrix(result_matrix_multiplication);
+  destroy_matrix(result_matrix_multiplication);
+
+  double* result_gaussian_elimination = call_gaussian_elimination(N);
+  free(result_gaussian_elimination);
 
   return 0;
 }
--- programme/sequential_algorithms.h
+++ programme/sequential_algorithms.h
@@ -3,9 +3,13 @@
 #ifndef SEQUENTIAL_ALGORITHMS_H
 #define SEQUENTIAL_ALGORITHMS_H
 
-int** createMatrix(size_t M, size_t N);
-void destroyMatrix(int** arr);
+// Helper
+int** create_matrix(size_t M, size_t N);
+void destroy_matrix(int** arr);
+void swap_row(double *a, double *b, int r1, int r2, int n);
 
+// Algorithms
 int** matrix_multiplication(int *matrix1, int *matrix2, size_t rows1, size_t columns1, size_t rows2, size_t columns2);
+void gaussian_elimination(double *a, double *b, double *x, int n);
 
 #endif
